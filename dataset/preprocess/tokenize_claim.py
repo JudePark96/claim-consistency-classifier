2993945e5df713d0f29bb99e472770b6d6398317
JudePark96 <judepark@kookmin.ac.kr>
2021-07-15 02:55:08 +0900

    fix: save json line by line

--- dataset/preprocess/tokenize_claim.py
+++ dataset/preprocess/tokenize_claim.py
@@ -13,15 +13,16 @@ def tokenize_claims_and_save(raw_claim_path: str, target_path: str) -> None:
         claim_set = [json.loads(i) for i in tqdm(f)]
 
     with open(target_path, 'a+', encoding='utf-8') as f:
-        for claim in claim_set:
-            claim['mutated'] = ' '.join(word_tokenize(claim['mutated']))
-            claim['original'] = ' '.join(word_tokenize(claim['original']))
+        for claim in tqdm(claim_set):
+            claim['mutated'] = ' '.join(word_tokenize(claim['mutated'])).strip()
+            claim['original'] = ' '.join(word_tokenize(claim['original'])).strip()
 
-            f.write(json.dumps(f'{claim}\n'))
+            f.write(json.dumps(claim) + '\n')
 
 
 if __name__ == '__main__':
-    raw_claim_path = glob('../../rsc/raw_claims/*')
+    raw_claim_path = glob('../../rsc/raw_claims/*.jsonl')
+    print(raw_claim_path)
     target_path = '../../rsc/raw_claims/nltk_tokenized/'
 
     for file_path in raw_claim_path:
